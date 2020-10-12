cc6b2aca3f038158c708bacd31baaf9d9d58490f
Alex Olle <alex.olle.93@gmail.com>
2020-10-12 18:33:09 +0200

    Name included, sign up and log in done, just red box left, added responsive to the nav-bar but not done yet

---
+++ js/User.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+class User {
+  constructor(name, coffee, email, password) {
+    this.name = name;
+    this.coffee = coffee;
+    this.email = email;
+    this.password = password;
+  }
+}
