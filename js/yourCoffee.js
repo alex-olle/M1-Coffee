648c2ed46d2b5812c3311c26325fb80d384bbb39
Alex Olle <alex.olle.93@gmail.com>
2020-10-13 18:17:20 +0200

    Some JS modifications

--- js/yourCoffee.js
+++ js/yourCoffee.js
@@ -4,16 +4,17 @@ let getSize = async () => {
     const data = await fetch (myApi);
     const arrData = await data.json();
     const selectElem = document.getElementsByClassName('size')
-    const arrElem = [...selectElem]
+    const arrElem = [...selectElem] 
+    console.log(document.querySelector('#size option').value)
     // console.log(arrElem[0].value)
-    arrElem.forEach( el => {
-        let filterName = el.value
-        // console.log(filterName)
-        let filteredArr = arrData.filter( fil => {
-            return fil.type === filterName;
-        })
-        console.log(filteredArr)
-    })
+    // arrElem.forEach( el => {
+    //     let filterName = el.value
+    //     // console.log(filterName)
+    //     let filteredArr = arrData.filter( fil => {
+    //         return fil.type === filterName;
+    //     })
+    //     console.log(filteredArr)
+    // })
 }
 
 // getSize();
