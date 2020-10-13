ffb6f83ac440389e1a5d00fe316acd2bb6d4336e
Alex Olle <alex.olle.93@gmail.com>
2020-10-13 17:31:29 +0200

    Little modifications HTML+CSS and first iterations with JS

--- js/yourCoffee.js
+++ js/yourCoffee.js
@@ -1,14 +1,28 @@
 const myApi = "http://coffe-app-ih-aor.herokuapp.com/pods"
 
 let getSize = async () => {
-    const size = await fetch (myApi);
-    const sizejson = await size.json();
-    const type = sizejson[0].type;
-    const flavour = sizejson[0].flavor;
-    console.log(type)
-    console.log(flavour)
+    const data = await fetch (myApi);
+    const arrData = await data.json();
+    const selectElem = document.getElementsByClassName('size')
+    const arrElem = [...selectElem]
+    // console.log(arrElem[0].value)
+    arrElem.forEach( el => {
+        let filterName = el.value
+        // console.log(filterName)
+        let filteredArr = arrData.filter( fil => {
+            return fil.type === filterName;
+        })
+        console.log(filteredArr)
+    })
 }
 
-getSize();
+// getSize();
+
+let getFlavour = async () => {
+    await getSize();
+
+}
+
+getFlavour()
 
 window.addEventListener('load', getSize);
