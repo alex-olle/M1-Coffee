c387a3415366ad830f232a21baf1528747f13a44
Alex Olle <alex.olle.93@gmail.com>
2020-10-14 11:37:25 +0200

    Some little modifications in CSS and JS object reached

--- js/yourCoffee.js
+++ js/yourCoffee.js
@@ -3,27 +3,32 @@ const myApi = "http://coffe-app-ih-aor.herokuapp.com/pods"
 let getSize = async () => {
     const data = await fetch (myApi);
     const arrData = await data.json();
-    const selectElem = document.getElementsByClassName('size')
-    const arrElem = [...selectElem] 
-    console.log(document.querySelector('#size option').value)
-    // console.log(arrElem[0].value)
-    // arrElem.forEach( el => {
-    //     let filterName = el.value
-    //     // console.log(filterName)
-    //     let filteredArr = arrData.filter( fil => {
-    //         return fil.type === filterName;
-    //     })
-    //     console.log(filteredArr)
-    // })
+    let sizeValue = document.querySelector('#size option').value
+    let sizeFilterArr = arrData.filter( el => {
+        return el.type === sizeValue;
+    })
+
+    return sizeFilterArr
 }
 
 // getSize();
 
 let getFlavour = async () => {
-    await getSize();
+    const sizeArr = await getSize();
+    console.log(sizeArr)
+    let flavourValue = document.querySelector('#flavour option').value
+    console.log(flavourValue)
+    let flavourFilterArr = sizeArr.filter( el => {
+        return el.flavor === flavourValue;
+    })
 
+    console.log(flavourFilterArr);
+    return flavourFilterArr[0]
 }
 
-getFlavour()
 
-window.addEventListener('load', getSize);
+
+// window.addEventListener('load', getFlavour);
+
+const findYourCoffeeBtn = document.getElementById('find-coffee-btn');
+findYourCoffeeBtn.addEventListener("click", getFlavour)
